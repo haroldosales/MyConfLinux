d4d0da8b62056c30fadfc03543df06ceb3a73230
haroldosales <haroldognu@gmail.com>
2020-08-30 18:42:46 -0300

    atualizar

--- arch.sh
+++ arch.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 
-sudo pacman -Syu  atom php base-devel yaourt git wget yajl jdk-openjdk  intellij-idea-community-edition firefox
+sudo pacman -Syu  code php base-devel yaourt git wget yajl jdk-openjdk  intellij-idea-community-edition firefox
 
 
 pamac install google-chrome 
 
-apm install linter-php terminal-tab ide-php language-javascript autocomplete power-mode
+
