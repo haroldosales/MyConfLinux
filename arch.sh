7b6f91e5e87c0d421ba97bb63a53875b24dd7a51
haroldosales <haroldognu@gmail.com>
2021-01-09 14:29:58 -0300

    atualizando

--- arch.sh
+++ arch.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 
-sudo pacman -Syu  atom  php base-devel  jdk-openjdk  intellij-idea-community-edition firefox chromium 
+#sudo pacman -Syu  atom  php base-devel  jdk-openjdk  intellij-idea-community-edition firefox  postgresql pgadmin
 
 
 apm install terminal-tab sync-settings seti-icons script php-debug minimap linter linter-php ide-php goto-definition emmet debug autoclose-html atom-beautify atom-autocomplete-php
 
- 
+ su passwd postgres
 
+su postgres
 
+initdb -D $lang  /var/lib/postgres/data/
 
