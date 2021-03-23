4342fffc6ba6ebc5e1c7810fdd52fc1be497ae72
haroldosales <haroldognu@gmail.com>
2021-03-23 20:40:48 -0300

    installUbuntuPcFirma

--- arch.sh
+++ arch.sh
@@ -3,12 +3,10 @@
 
 #sudo pacman -Syu  atom  php base-devel  jdk-openjdk  intellij-idea-community-edition firefox  postgresql pgadmin
 
-
 apm install terminal-tab sync-settings seti-icons script php-debug minimap linter linter-php ide-php goto-definition emmet debug autoclose-html atom-beautify atom-autocomplete-php
 
- su passwd postgres
+# su passwd postgres
 
-su postgres
+#su postgres
 
-initdb -D $lang  /var/lib/postgres/data/
-
+#initdb -D $lang  /var/lib/postgres/data/
---
+++ installUbunt.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+sudo apt update && sudo apt upgrade
+
+sudo apt install build-essential php* atom python* vscode atom  jdk-openjdk  intellij-idea-community-edition firefox  postgresql pgadmin
+
+apm install terminal-tab sync-settings seti-icons script php-debug minimap linter linter-php ide-php goto-definition emmet debug autoclose-html atom-beautify atom-autocomplete-php
