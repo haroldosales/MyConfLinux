4d8514130aee3f5ca3dd6e67119582b806c0b641
haroldosales <haroldognu@gmail.com>
2022-12-04 18:19:56 -0300

    update

--- arch.sh
+++ arch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-#sudo pacman -Syu  atom  php base-devel  jdk-openjdk  intellij-idea-community-edition firefox  postgresql pgadmin
+sudo pacman -Syu    php base-devel  jdk-openjdk  intellij-idea-community-edition firefox  postgresql pgadmin
 
 apm install terminal-tab sync-settings seti-icons script php-debug minimap linter linter-php ide-php goto-definition emmet debug autoclose-html atom-beautify atom-autocomplete-php atom-live-server
 
