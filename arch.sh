c18d39b777480811d8b876950aba2950f0be0147
haroldosales <haroldognu@gmail.com>
2024-03-31 19:10:36 -0300

    update and create swap

--- arch.sh
+++ arch.sh
@@ -18,7 +18,12 @@
 
 
 # ------------------VARIAVEIS-----------------------------------#
-
+CREATESWAPFILE="SWAP FILE"
+SWAP=`sudo  swapoff /swapfile
+      sudo  dd if=/dev/zero of=/swapfile bs=1M count=14k status=progress
+	sudo  chmod 0600 /swapfile
+       sudo mkswap =U clear /swapfile
+      sudo swapon /swapfile`
 MENSAGEM="Install pacman"
 INSTALLPACMAN="sudo pacman -Syu php base-devel  jdk-openjdk  intellij-idea-community-edition nodejs npm discord git obs-studio vlc"
 MENSAGEM2="Install Pamac"
@@ -57,8 +62,10 @@ do
     -v) echo "$VERSAO" && exit 0                     ;;
     -s) echo "install pacman" && $INSTALLPACMAN      ;;
     -m) echo "install pamac" && $INSTALLPAMAC        ;;
+    -x) echo "SWAP" && $SWAP                         ;;
      *) echo "Opção inválida, valie o -h." && exit 1 ;;
-  esac
+ 
+ esac
   shift
 done
 
